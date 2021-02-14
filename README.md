aca3b775e0ad9d95fc0a75c9c32c51f040ee30bb
xiaowenli95 <xiaowen95.li@gmail.com>
2021-02-14 23:11:12 +0100

    An example on how to get a players match history by summoner name

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # riot
 # This is from R studio
+# 20 requests every 1 seconds(s)
+# 100 requests every 2 minutes(s)
--- access_api.R
+++ access_api.R
@@ -1,1 +1,32 @@
 library(dplyr)
+library(httr)
+library(jsonlite)
+
+# acess api
+key <- "RGAPI-a3bed222-98d3-41b1-b302-63c8ca7b25f0"
+# routed url syntax e.g. https://api-id.execute-api.us-east-2.amazonaws.com/pets/6.
+
+# summoner stats
+summoner_name <- "Wildwo0olf"
+url1 <- paste("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/", summoner_name, "?api_key=", key, sep = "")
+# the returning data format is json
+return1 <- GET(url1)
+http_type(return)
+# get the id/puuid
+content1 <- content(return, as = "parsed")
+account_id <- content$accountId
+
+# matches history
+url2 <- paste("https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/", account_id, "?api_key=", key, sep = "")
+return2 <- GET(url2)
+content2_parsed <- content(return2, as = "parsed") 
+content2 <- content(return2, as = "text") 
+matches <- fromJSON(content2)$matches
+  
+  
+  
+  
+  
+  
+
+# url <- paste("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-account/", summoner, "?api_key=", key, sep = "")
