0e7de4b988d9796df11b47f14c36ba8000035106
xiaowenli95 <xiaowen95.li@gmail.com>
2021-02-15 23:04:14 +0100

    Abstracted a get_content function to communicate with api. Get the match_id (== game_id). MOdified readme file format.

--- access_api.R
+++ access_api.R
@@ -1,32 +1,34 @@
-library(dplyr)
+library(tidyverse)
 library(httr)
 library(jsonlite)
 
-# acess api
-key <- "RGAPI-a3bed222-98d3-41b1-b302-63c8ca7b25f0"
+# access api key, stored locally
+source("C:/Users/jmlhz/Documents/api_key.R")
 # routed url syntax e.g. https://api-id.execute-api.us-east-2.amazonaws.com/pets/6.
 
-# summoner stats
+# get content function that gets the content of each api access
+get_content <- function(url_component, identifier, as = "parsed"){
+  url <- paste(url_component, identifier, "?api_key=", key, sep = "")
+  return <- GET(url)
+  print(paste("Return data type is ", http_type(return), sep = ""))
+  content <- content(return, as = as)
+  return(content)
+}
+
+# summoner account id
 summoner_name <- "Wildwo0olf"
-url1 <- paste("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/", summoner_name, "?api_key=", key, sep = "")
-# the returning data format is json
-return1 <- GET(url1)
-http_type(return)
-# get the id/puuid
-content1 <- content(return, as = "parsed")
-account_id <- content$accountId
+account_id<- get_content("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/", summoner_name)$accountId
 
-# matches history
-url2 <- paste("https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/", account_id, "?api_key=", key, sep = "")
-return2 <- GET(url2)
-content2_parsed <- content(return2, as = "parsed") 
-content2 <- content(return2, as = "text") 
-matches <- fromJSON(content2)$matches
-  
-  
-  
-  
-  
-  
+# matches history of the specified summoner
+matches <- fromJSON(get_content("https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/", account_id, as = "text"))$matches
+matches_cleaned <- matches %>%
+  mutate(date = as.POSIXct(timestamp/1000, origin = "1970-01-01"))
 
+# historical stats of each match
+
+  
+  
+  
+  
+## draft below
 # url <- paste("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-account/", summoner, "?api_key=", key, sep = "")
