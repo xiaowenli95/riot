2653ec67a2e3179496440390266b70caa10e35ba
xiaowenli95 <xiaowen95.li@gmail.com>
2021-02-19 14:26:14 +0100

    Fetched json data of all champion spells and passive. Some champions has some entries unexpectedly null.

--- champions_df.R
+++ champions_df.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
 current_patch <- "11.4.1"
-champion_url <- paste("http://ddragon.leagueoflegends.com/cdn/", current_patch, 
+champion_summary_url <- paste("http://ddragon.leagueoflegends.com/cdn/", current_patch, 
                       "/data/en_US/champion.json", sep = "")
-champion_summary <- content(GET(champion_url), as = "parsed")
+champion_summary <- content(GET(champion_summary_url), as = "parsed")
 jsonedit(champion_summary, mode = "view")
 
 # as a first step, the dataframe needs columns "id, key, tags, stats"
@@ -36,6 +36,24 @@ champions_df <- v4 %>%
   inner_join(v2, by = "champion_name") %>%
   inner_join(v3_1, by = "champion_name") %>%
   inner_join(v3_2, by = "champion_name")
-  
+
+# enrich champion data with specifics (spells and passive)
+get_champion_specifics <- function(champion_name, current_patch = "11.4.1"){
+  current_patch <- current_patch
+  champion_url <- paste("http://ddragon.leagueoflegends.com/cdn/", current_patch, 
+                                "/data/en_US/champion/", champion_name, ".json", sep = "")
+  champion_summary <- content(GET(champion_url), as = "parsed")$data
+  Sys.sleep(0.05)
+  return(champion_summary)
+}
+champion_specifics <- list()
+for (i in champions_df$champion_name[101:length(champions_df$champion_name)]){
+  champion_specifics[[i]] <- get_champion_specifics(i)
+}
+# save(champion_specifics, file = "data/champion_specifics.RData")
+
+# qa: Akali Q has no spell value in effect burn variable
+jsonedit(champion_specifics[[3]], mode = "view")
+
 # is champion key same as champion id?
 load("data/matches_game.RData")
