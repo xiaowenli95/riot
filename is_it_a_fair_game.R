fbe068ae67149a0ea12d84e32cbba9f50e06967f
xiaowenli95 <xiaowen95.li@gmail.com>
2021-02-19 11:51:27 +0100

    Start working on champion stats

--- is_it_a_fair_game.R
+++ is_it_a_fair_game.R
@@ -15,14 +15,16 @@ their_win_rate <- function(summoner_name, they = "team"){
   my_win_rate <- get_win_rate(summoner_name, players_df)
   i_played_with <- who_i_play_with(summoner_name, players_df)
   team <- i_played_with[["team"]] %>%
-    slice(1:10) %>%
     pull(1)
   their_win_rate <- list()
   m = 1
   for (i in team){
+    tryCatch({
     players_df <- get_players_dataframe(i)
+    save(players_df, file = paste("data/players_df_", i, ".RData", sep = ""))
     their_win_rate[[m]] <- get_win_rate(i, players_df)
-    m = m + 1
+    m = m + 1}, 
+    error = function(e){cat("ERROR :", i ,conditionMessage(e), "\n")})
   }
   return(their_win_rate)
 }
