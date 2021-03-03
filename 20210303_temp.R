3b24a96d6b344bc786447ee08858e05bdf29563c
xiaowenli95 <xiaowen95.li@gmail.com>
2021-03-03 19:43:11 +0100

    # Created and unioned 10 players matches info dataframe. Found that get_matches_dataframe is not robust enough when 'stats' don't have specified variables, which is partially fixed.

--- 20210303_temp.R
+++ 20210303_temp.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 # get 200 players match info
 
 # get one player's opponents and teammates
@@ -15,3 +17,34 @@ for (i in as.matrix(a[['opponent']])){
   match_id <- get_matchid(account_id)
   a <- get_matches_info(summoner_name, match_id)
 }
+
+# create and union matches_df datasets
+matches_info_list <- list.files("./data") %>%
+  str_subset("^.*(?=_matches_info)")
+df_list <- list()
+# load the data transformation function
+source("./matches_df.R")
+queue_info_list <- list()
+matches_list <- list()
+
+for (i in matches_info_list){
+  file_name <- i
+  try({
+    # load matches_game list
+    load(paste("./data/", file_name, sep = ""))
+    # get the list of dataframes
+    b <- get_matches_dataframe(match_info = matches_game)
+    # separate dataframes
+    queue_info_list[[file_name]] <- b$"queue_info_df"
+    matches_list[[file_name]] <- b$"matches_df"
+  })
+}
+
+# create data frames
+matches_df <- bind_rows(matches_list) %>%
+  # drop duplicates
+  select(!contains("."))
+queue_info_df <- bind_rows(queue_info_list)
+
+save(matches_df, queue_info_df, file = "./data/20210303_some_players_match_history.Rdata")
+
