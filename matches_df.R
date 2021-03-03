3f880f1dd12baa60e560aa8ed029c21971f71941
xiaowenli95 <xiaowen95.li@gmail.com>
2021-03-03 19:43:50 +0100

    # The partial fix is still not robust enough. Duplicated columns are created.

--- matches_df.R
+++ matches_df.R
@@ -34,10 +34,10 @@
 library(tidyverse)
 library(listviewer)
 
-load("./data/ Wildwo0olf _matches_info.RData")
-jsonedit(matches_game, mode = "view")
+# load("./data/ Wildwo0olf _matches_info.RData")
+# # jsonedit(matches_game, mode = "view")
 
-get_match_info <- function(match_info){
+get_matches_dataframe <- function(match_info){
   # first fetch all variables into lists, then bind rows to transform them into a dataframe
   # get third hierarchy. Dataframe of queue info
   queue_info_var <- c("gameId", "queueId", "gameDuration", "mapId")
@@ -90,7 +90,8 @@ get_match_info <- function(match_info){
     # extraction
     map(~map(.x, ~map(.x, ~map(.x, ~.x[player_var_2])))) %>%
     # bind rows
-    map_dfr(~map_dfr(.x, ~map_dfr(.x, ~map_dfr(.x, ~as.data.frame(.x)))), .id = "matchid")
+    # some variables in 'stats' are missing for some players. Therefore 'keep' is used to filter out NULL items
+    map_dfr(~map_dfr(.x, ~map_dfr(.x, ~map_dfr(.x, ~as.data.frame(keep(.x, ~!is.null(.x)))))), .id = "matchid")
   
   # merge temp tables
   matches_df <- temp1 %>%
@@ -100,5 +101,13 @@ get_match_info <- function(match_info){
   return(a)
 }
 
-a <- get_match_info(matches_game)
-          
+# stats length are the same even when there is a null item
+# t3 <- unlist(map(temp2, ~map(.x, ~map(.x, ~map(.x, ~length(.x))))))
+# keep(t3, ~.x != 21)
+# 
+# t4 <- unlist(map(temp2, ~map(.x, ~map(.x, ~length(.x)))))
+# keep(t4, ~.x != 1)
+# 
+# t5 <- unlist(map(temp2, ~map(.x, ~length(.x))))
+# keep(t5, ~.x != 1)
+# purrr::flatten(match_info[48][[1]][['participants']][[1]][['stats']])
